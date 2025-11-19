e2fba3a5d226101f04b096ae59c434021c60dbbe
Manasa <66729634+manasapuellela@users.noreply.github.com>
2025-11-19 12:42:09 -0500

    Update extract_audio.py

--- utils/extract_audio.py
+++ utils/extract_audio.py
@@ -1,1 +1,8 @@
+import moviepy.editor as mp
+
+def extract_audio_from_video(video_path, output_path="output_audio.wav"):
+    video = mp.VideoFileClip(video_path)
+    audio = video.audio
+    audio.write_audiofile(output_path)
+    return output_path
 
