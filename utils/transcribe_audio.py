a6be3aad7ddda49f1346d63e51bffe87088682a8
Manasa <66729634+manasapuellela@users.noreply.github.com>
2025-11-19 12:42:45 -0500

    Add transcription utility

--- utils/transcribe_audio.py
+++ utils/transcribe_audio.py
@@ -1,1 +1,17 @@
+import openai
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+def transcribe_audio(audio_path):
+    audio_file = open(audio_path, "rb")
+
+    transcript = openai.audio.transcriptions.create(
+        model="whisper-1",
+        file=audio_file
+    )
+
+    return transcript.text
 
